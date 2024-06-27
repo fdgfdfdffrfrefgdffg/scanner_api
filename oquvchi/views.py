36e15977737eb6b8bd923048f0cc51c2a90b96f4
odildev007 <164607202+odildev007@users.noreply.github.com>
2024-06-27 22:20:02 +0500

    Update views.py

--- oquvchi/views.py
+++ oquvchi/views.py
@@ -47,37 +47,38 @@ class FaceCompareView(APIView):
         return Response({"message": "Bu yerga faqat post qiling."})
     def post(self, request, *args, **kwargs):   
         if request.FILES["file"]:
-            rasm = request.FILES["file"]
-            img_path = f"media/{time()}{rasm.name}"
-            
-            with open(img_path, "wb") as f:
-                f.write(rasm.read())
-            
-            rasm_url = f"https://maktab-davomat.uz/" + img_path
-            api_key = 'hRKyY_ca3CmuOl4BVYAFa1D1cETKckFT'
-            api_secret = 'Tn0KO__Zes3bD_Nxpoqgs4L8Wwok7rax'
-            app_ = FacePP(api_key=api_key, api_secret=api_secret)
-            res = {"topilganlar": []}
-            # O'quvchilarni tekshirish
-            for oquvchi in Oquvchi.objects.all():
-                # try:
-                    # Face++ compare funksiyasini chaqirish
-                    cmp_ = app_.compare.get(image_url1=img_path, image_url2="https://maktab-davomat.uz/"+ oquvchi.face_url)
+            try:
+                rasm = request.FILES["file"]
+                img_path = f"media/{time()}{rasm.name}"
+                
+                with open(img_path, "wb") as f:
+                    f.write(rasm.read())
+                
+                rasm_url = f"https://maktab-davomat.uz/" + img_path
+                api_key = 'hRKyY_ca3CmuOl4BVYAFa1D1cETKckFT'
+                api_secret = 'Tn0KO__Zes3bD_Nxpoqgs4L8Wwok7rax'
+                app_ = FacePP(api_key=api_key, api_secret=api_secret)
+                res = {"topilganlar": []}
+                # O'quvchilarni tekshirish
+                for oquvchi in Oquvchi.objects.all():
+                    # try:
+                        # Face++ compare funksiyasini chaqirish
+                        cmp_ = app_.compare.get(image_url1=img_path, image_url2="https://maktab-davomat.uz/"+ oquvchi.face_url)
 
 
-                    # Agar yuzlar mos kelsa, o'quvchi ma'lumotlarini qaytarish
-                    if cmp_.confidence > 70:
-                        res["topilganlar"].append({
-                            "ism_familya": oquvchi.ism_familya,
-                            "sinf": oquvchi.sinf,
-                            "maktab": oquvchi.maktab,
-                            "Telefon_raqam": oquvchi.phone,
-                            "manzil": oquvchi.manzil,
-                        })
+                        # Agar yuzlar mos kelsa, o'quvchi ma'lumotlarini qaytarish
+                        if cmp_.confidence > 70:
+                            res["topilganlar"].append({
+                                "ism_familya": oquvchi.ism_familya,
+                                "sinf": oquvchi.sinf,
+                                "maktab": oquvchi.maktab,
+                                "Telefon_raqam": oquvchi.phone,
+                                "manzil": oquvchi.manzil,
+                            })
 
-                # except exceptions.BaseFacePPError as e:
-                #     # Agar Face++ da xato bo'lsa, xato xabarini qaytarish
-                #     return Response({"message": str(e), "images":f"{rasm_file} = {oquvchi.face_url}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            except exceptions.BaseFacePPError as e:
+                # Agar Face++ da xato bo'lsa, xato xabarini qaytarish
+                return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             else:
                 os.remove(img_path)
                 return Response(res) if res else Response({"message": "O'xshash yuz topilmadi."}, status=status.HTTP_404_NOT_FOUND)
